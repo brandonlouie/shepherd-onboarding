97e5a04f49bd51ead1efdc0ef955d4f5c98a2def
Brandon Louie <brandon.louie@berkeley.edu>
2021-10-20 20:46:02 -0700

    draft of my website

--- MINI-PROJ-1/student/views.py
+++ MINI-PROJ-1/student/views.py
@@ -7,9 +7,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    pass
+    return render_template("your_code.html")
     # Your code here
 
+@app.route('/about_me')
+def route1func():
+    return render_template("about_me.html")
+
+@app.route('/route2')
+def route2func():
+    return render_template("route2wip.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
